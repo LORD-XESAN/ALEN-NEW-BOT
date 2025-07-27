e9fcfe513b5c911630753b9e658309a350e36bd0
AI4004 <tk545198@gmail.com>
2025-07-27 15:29:16 +0600

    Update unsend.js

--- Script/commands/unsend.js
+++ Script/commands/unsend.js
@@ -12,16 +12,23 @@ module.exports.config = {
 module.exports.languages = {
 	"vi": {
 		"returnCant": "Không thể gỡ tin nhắn của người khác.",
-		"missingReply": "Hãy reply tin nhắn cần gỡ."
+		"missingReply": "Hãy reply tin nhắn cần gỡ.",
+		"emojiTrigger": "Để gỡ tin nhắn, vui lòng gửi 😡."
 	},
 	"en": {
 		"returnCant": "আরে বলদ অন্য কারো মেসেজ আমি আনসেন্ড করবো কিভাবে পাগল ছাগল",
-		"missingReply": "আপনি আমার কোন মেসেজটি আনসেন্ড করবেন , তা রিপ্লাই করুন 🌺"
+		"missingReply": "আপনি আমার কোন মেসেজটি আনসেন্ড করবেন, তা রিপ্লাই করুন 🌺",
+		"emojiTrigger": "To unsend a message, please send 😡."
 	}
 }
 
 module.exports.run = function({ api, event, getText }) {
-	if (event.messageReply.senderID != api.getCurrentUserID()) return api.sendMessage(getText("returnCant"), event.threadID, event.messageID);
-	if (event.type != "message_reply") return api.sendMessage(getText("missingReply"), event.threadID, event.messageID);
-	return api.unsendMessage(event.messageReply.messageID);
+	if (event.messageReply) {
+		if (event.messageReply.senderID != api.getCurrentUserID()) return api.sendMessage(getText("returnCant"), event.threadID, event.messageID);
+		return api.unsendMessage(event.messageReply.messageID);
+	} else if (event.body === "😡") {
+		return api.sendMessage(getText("emojiTrigger"), event.threadID, event.messageID);
+	} else {
+		return api.sendMessage(getText("missingReply"), event.threadID, event.messageID);
 	}
+}
